cee0b6c40beab74cb2d0a82287fbdb30a7c0cf38
P <nguyenanphuc92@gmail.com>
2020-04-13 21:22:29 +0700

    Add Carousels

--- src/components/views/Main/Main.js
+++ src/components/views/Main/Main.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import BookList from './BookList';
+import NewBooks from './NewBooks';
+import Carousels from '../Base/Carousels/Carousels';
 
 class Main extends Component {
     
     render() {
         return (
-            <div>Main view ...</div>
+            <div>
+                <Carousels></Carousels>
+                <BookList></BookList>
+                <NewBooks></NewBooks> 
+            </div>
         );
     }
 }
---
+++ src/components/views/Main/NewBooks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NewBooks extends Component {
+    
+    render() {
+        return (
+            <div>
+                New books ...
+            </div>
+        );
+    }
+}
+
+export default NewBooks;
